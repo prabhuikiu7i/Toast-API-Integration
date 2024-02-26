6e66b1acd72e2c00838736d8b22b8acc85cd10f4
prabhuikiu7i <prabhuvenkatesh8561@gmail.com>
2024-02-26 18:17:21 +0530

    changes

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const Client = require('pg').Client;
 const app = express();
+const port = 8080;
 
+const client = new Client({
+    host: '3.108.67.115',
+    user: 'postgres',
+    port: 5432,
+    password: 'ResPsql987',
+    database: 'postgres'
+});
 
-// const client = new Client({
-//     host: '3.108.67.115',
-//     user: 'postgres',   
-//     port: 5432,
-//     password: 'ResPsql987',
-//     database: 'postgres'
-// });
-
-// client.connect();
+client.connect();
 
 const merchantId = 'JH1T8ZPBVPS71';
 app.use(cors());
@@ -32,59 +32,59 @@ app.get('/', async (req, res) => {
 });
 
 
-// app.post('/webhook', async (req, res) => {
-//     const webhookData = req.body;
-//     try {
-//         if (webhookData && webhookData.merchants) {
-//             for (const merchantId in webhookData.merchants) {
-//                 const events = webhookData.merchants[merchantId];
+app.post('/webhook', async (req, res) => {
+    const webhookData = req.body;
+    try {
+        if (webhookData && webhookData.merchants) {
+            for (const merchantId in webhookData.merchants) {
+                const events = webhookData.merchants[merchantId];
 
-//                 for (const event of events) {
-//                     const objectId = event.objectId;
-//                     const type = event.type;
+                for (const event of events) {
+                    const objectId = event.objectId;
+                    const type = event.type;
 
-//                     if (type === 'CREATE') {
+                    if (type === 'CREATE') {
 
-//                         const itemResponse = await axios.get(`https://apisandbox.dev.clover.com/v3/merchants/${merchantId}/items/${objectId}`);
-//                         const item = itemResponse.data;
+                        const itemResponse = await axios.get(`https://apisandbox.dev.clover.com/v3/merchants/${merchantId}/items/${objectId}`);
+                        const item = itemResponse.data;
 
-//                         const insertQuery = {
-//                             text: 'INSERT INTO CloverTable (id, Item_Name, Price, Price_Type, Taxes_And_Fees, Item_Color, type) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-//                             values: [item.id, item.name, item.price, item.priceType, item.defaultTaxRates, item.isRevenue, 'clover'],
-//                         };
+                        const insertQuery = {
+                            text: 'INSERT INTO CloverTable (id, Item_Name, Price, Price_Type, Taxes_And_Fees, Item_Color, type) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+                            values: [item.id, item.name, item.price, item.priceType, item.defaultTaxRates, item.isRevenue, 'clover'],
+                        };
 
-//                         await client.query(insertQuery);
-//                     } else if (type === 'UPDATE') {
+                        await client.query(insertQuery);
+                    } else if (type === 'UPDATE') {
 
-//                         const itemResponse = await axios.get(`https://apisandbox.dev.clover.com/v3/merchants/${merchantId}/items/${objectId}`);
-//                         const item = itemResponse.data;
+                        const itemResponse = await axios.get(`https://apisandbox.dev.clover.com/v3/merchants/${merchantId}/items/${objectId}`);
+                        const item = itemResponse.data;
 
-//                         const updateQuery = {
-//                             text: 'UPDATE CloverTable SET Item_Name = $2, Price = $3, Price_Type = $4, Taxes_And_Fees = $5, Item_Color = $6, type = $7 WHERE id = $1',
-//                             values: [item.id, item.name, item.price, item.priceType, item.defaultTaxRates, item.isRevenue, 'UPDATE'],
-//                         };
-//                         await client.query(updateQuery);
-//                     } else if (type === 'DELETE') {
+                        const updateQuery = {
+                            text: 'UPDATE CloverTable SET Item_Name = $2, Price = $3, Price_Type = $4, Taxes_And_Fees = $5, Item_Color = $6, type = $7 WHERE id = $1',
+                            values: [item.id, item.name, item.price, item.priceType, item.defaultTaxRates, item.isRevenue, 'UPDATE'],
+                        };
+                        await client.query(updateQuery);
+                    } else if (type === 'DELETE') {
 
-//                         const deleteQuery = {
-//                             text: 'DELETE FROM CloverTable WHERE id = $1',
-//                             values: [item.id],
-//                         };
-//                         await client.query(deleteQuery);
-//                     }
-//                 }
-//             }
-//             res.json({ success: true });
-//         } else {
-//             res.status(400).json({ error: 'Invalid webhook data format' });
-//         }
-//     } catch (error) {
-//         console.error("Error handling webhook:", error.message);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
+                        const deleteQuery = {
+                            text: 'DELETE FROM CloverTable WHERE id = $1',
+                            values: [item.id],
+                        };
+                        await client.query(deleteQuery);
+                    }
+                }
+            }
+            res.json({ success: true });
+        } else {
+            res.status(400).json({ error: 'Invalid webhook data format' });
+        }
+    } catch (error) {
+        console.error("Error handling webhook:", error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 
-app.listen( () => {
-    console.log(`Server is running on http://3.108.67.115`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
